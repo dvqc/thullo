19fac880fbb4ee9d302f48152c4fe96bcb419b28
rahimbtc <rahim.bait.2021@gmail.com>
2023-03-23 21:49:03 +0100

    changes

--- src/components/layout/content.tsx
+++ src/components/layout/content.tsx
@@ -6,19 +6,25 @@ const users = [{ name: "A" }, { name: "B" }, { name: "C" }];
 
 const Content = () => {
   return (
-    <div className="flex space-x-4 p-4">
-      <div className="flex items-center space-x-2 rounded-md bg-gray-100 p-2">
-        <div>
+    <div className="flex justify-between space-x-4 p-4">
+      <div className="flex space-x-3">
+        <div className="flex items-center space-x-2 rounded-md bg-gray-100 p-2">
           <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 96 960 960" width="18">
             <path d="M220 976q-24.75 0-42.375-17.625T160 916V482q0-24.75 17.625-42.375T220 422h70v-96q0-78.85 55.606-134.425Q401.212 136 480.106 136T614.5 191.575Q670 247.15 670 326v96h70q24.75 0 42.375 17.625T800 482v434q0 24.75-17.625 42.375T740 976H220Zm0-60h520V482H220v434Zm260.168-140Q512 776 534.5 753.969T557 701q0-30-22.668-54.5t-54.5-24.5Q448 622 425.5 646.5t-22.5 55q0 30.5 22.668 52.5t54.5 22ZM350 422h260v-96q0-54.167-37.882-92.083-37.883-37.917-92-37.917Q426 196 388 233.917 350 271.833 350 326v96ZM220 916V482v434Z" />
           </svg>
+          <p>Private</p>
         </div>
-        <div>Private</div>
+        <div className="flex space-x-3">
+          {users.map((user, index) => (
+            <Image className="h-11 w-11 rounded" src={ProfilePic} alt={user.name} key={index} />
+          ))}
+        </div>
       </div>
-      <div className="flex space-x-4">
-        {users.map((user, index) => (
-          <Image className="h-11 w-11 rounded" src={ProfilePic} alt={user.name} key={index} />
-        ))}
+      <div className="flex items-center space-x-2 rounded-md bg-gray-100 p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 96 960 960" width="25">
+          <path d="M207.858 624Q188 624 174 609.858q-14-14.141-14-34Q160 556 174.142 542q14.141-14 34-14Q228 528 242 542.142q14 14.141 14 34Q256 596 241.858 610q-14.141 14-34 14Zm272 0Q460 624 446 609.858q-14-14.141-14-34Q432 556 446.142 542q14.141-14 34-14Q500 528 514 542.142q14 14.141 14 34Q528 596 513.858 610q-14.141 14-34 14Zm272 0Q732 624 718 609.858q-14-14.141-14-34Q704 556 718.142 542q14.141-14 34-14Q772 528 786 542.142q14 14.141 14 34Q800 596 785.858 610q-14.141 14-34 14Z" />
+        </svg>
+        <p>Show Menu</p>
       </div>
     </div>
   );
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,17 @@
+import React from "react";
+
 import Header from "../header";
 import Content from "./content";
 
-const Layout = () => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen">
+    <>
       <Header />
-      <main className="container mx-auto flex py-5">
+      <main className="container mx-auto flex flex-col py-5">
         <Content />
+        <div className="m-4 h-screen rounded-xl bg-[#F8F9FD] p-5">{children}</div>
       </main>
-    </div>
+    </>
   );
 };
 
