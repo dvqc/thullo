cee84dfaa21a9649efa17cd31b5d2600fcf9a540
rahimbtc <rahim.bait.2021@gmail.com>
2023-03-24 11:56:51 +0100

    just another idea about structure

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,13 +5,13 @@ import Content from "./content";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div>
       <Header />
-      <main className="container mx-auto flex flex-col py-5">
+      <main className="container mx-auto flex flex-col">
         <Content />
         <div className="m-4 h-screen rounded-xl bg-[#F8F9FD] p-5">{children}</div>
       </main>
-    </>
+    </div>
   );
 };
 
