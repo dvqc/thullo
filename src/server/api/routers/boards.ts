919fadc7f140b135b3725210deefea235d56a507
rahimbtc <rahim.bait.2021@gmail.com>
2023-04-03 19:53:04 +0200

    having prob with mutation

--- src/server/api/routers/boards.ts
+++ src/server/api/routers/boards.ts
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "~/server/api/trpc";
 import { content } from "tailwind.config.cjs";
+
+const MAX_FILE_SIZE = 500000;
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 
 export const boardRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
@@ -17,17 +23,31 @@ export const boardRouter = createTRPCRouter({
     if (!board) throw new TRPCError({ code: "NOT_FOUND" });
 
     return board;
-  })
+  }),
 
-  // create: protectedProcedure.mutation(async ({ ctx, input }) => {
-  //   // const authorId = ctx.userId;
-  //   const board = await ctx.prisma.board.create({
-  //     data: {
-  //       // userId: authorId,
-  //       content: input.content
-  //     }
-  //   });
+  create: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        isPrivate: z.boolean(),
+        picture: z
+          .any()
+          .refine((files) => files?.[0]?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+          .refine(
+            (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+            "Only .jpg, .jpeg, .png and .webp formats are supported."
+          )
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      // const authorId = ctx.userId;
+      const board = await ctx.prisma.board.create({
+        data: {
+          // userId: authorId,
+        }
+      });
 
-  //   return board;
-  // })
+      return board;
+    })
 });
